7cd51827c348d34aef03d46f231df3b79a15793c
Vimal <vimalkumarak478@gmail.com>
2025-09-24 16:26:00 +0530

    products fetch all data vkc

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import Products from '../components/Products';
 
 const Home = () => {
   return (
     <div>
       <h3 className='heading'>Welcome to Redux toolkit store</h3>
+      <section>
+        <h3>Product</h3>
+        <Products/>
+      </section>
     </div>
   )
 }
 
-export default Home
+export default Home;
