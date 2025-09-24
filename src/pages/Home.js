b50bbffae565a08498b15ca9cb99332aed64270f
Vimal <vimalkumarak478@gmail.com>
2025-09-24 16:08:55 +0530

    heading style vkc

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      This is home page.
+      <h3 className='heading'>Welcome to Redux toolkit store</h3>
     </div>
   )
 }
