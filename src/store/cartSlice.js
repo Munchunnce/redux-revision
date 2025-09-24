be7509d518bbc616fce4f0def04c20fbac4904f0
Vimal <vimalkumarak478@gmail.com>
2025-09-24 18:05:01 +0530

    create productSlice fetching data vkc

--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -9,7 +9,7 @@ export const cartSlice = createSlice({
             state.push(action.payload);
         },
         remove(state, action){
-            return state.filter((item) => item !== action.payload);
+            return state.filter((item) => item.id !== action.payload);
         }
     },
 });
