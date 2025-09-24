edfa724d54a63cb857ff575dbea9497a58793be2
Vimal <vimalkumarak478@gmail.com>
2025-09-24 15:49:29 +0530

    Navigation create page vkc

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Navigation = () => {
   return (
     <div>
-      This is navigation page
+      <span className='logo'>REDUX STORE</span>
+      <div>
+        <Link className='navLink' to='/'>Home</Link>
+        <Link className='navLink' to='/cart'>Cart</Link>
+        <span className='cartCount'>Cart items: 0</span>
+        <button className='btn'>Add to cart</button>
+      </div>
     </div>
   )
 }
