4adf2ed0fea1f6e5ade4f378fc81d6136544d517
Vimal <vimalkumarak478@gmail.com>
2025-09-24 17:00:57 +0530

    store create file vkc

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import store from './store/store';
 import Home from './pages/Home';
 import Cart from './pages/Cart';
 import Navigation from './components/Navigation';
+import { Provider } from 'react-redux';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-      <Navigation/>
-        <Routes>
-          <Route path='/' exact element={<Home/>}></Route>
-          <Route path='/cart' element={<Cart/>}></Route>
-        </Routes>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Navigation/>
+            <Routes>
+              <Route path='/' exact element={<Home/>}></Route>
+              <Route path='/cart' element={<Cart/>}></Route>
+            </Routes>
+        </BrowserRouter>
+      </Provider>
     </div>
   );
 }
