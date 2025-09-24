a48b49a43e035bf77144dcdc8119dc742320b98a
Vimal <vimalkumarak478@gmail.com>
2025-09-24 18:08:41 +0530

    prodcuts all data fetch vkc

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,20 +1,22 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux';
 import { add } from '../store/cartSlice'
+import { fetchProducts } from '../store/productSlice';
 
 const Products = () => {
     const [products, setProducts] = useState([]);
     const dispatch = useDispatch();
     
     useEffect(() => {
-        const fetchProducts = async () => {
-            const res = await fetch('https://fakestoreapi.com/products');
-            const data = await res.json();
-            setProducts(data);
-        };
+      dispatch(fetchProducts());
+        // const fetchProducts = async () => {
+        //     const res = await fetch('https://fakestoreapi.com/products');
+        //     const data = await res.json();
+        //     setProducts(data);
+        // };
 
-        fetchProducts();
-    });
+        // fetchProducts();
+    }, []);
 
     const handleAdd = (product) => {
       dispatch(add(product));
