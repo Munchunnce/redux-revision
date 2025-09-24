fdcfa817fd997e308e03fc56f303e663c16fdde1
Vimal <vimalkumarak478@gmail.com>
2025-09-24 15:55:09 +0530

    app.js file route render vkc

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Navigation/>
+        <Routes>
+          <Route path='/' exact element={<Home/>}></Route>
+          <Route path='/cart' element={<Cart/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
